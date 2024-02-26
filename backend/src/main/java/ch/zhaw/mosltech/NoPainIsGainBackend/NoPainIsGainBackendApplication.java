c2c07cf644dac3bee26dd9c08a37f44ca6c90394
peterheinrich <peter.heinrich@its-heinrich.ch>
2024-02-26 14:37:34 +0100

    Initial Commit

---
+++ backend/src/main/java/ch/zhaw/mosltech/NoPainIsGainBackend/NoPainIsGainBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.mosltech.NoPainIsGainBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoPainIsGainBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoPainIsGainBackendApplication.class, args);
+	}
+
+}
